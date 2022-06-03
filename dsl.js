c4ce969f2cd34225c4ca52cf261024f86d050164
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2022-06-02 19:35:25 -0500

    Improve asyncEngine support

--- dsl.js
+++ dsl.js
@@ -36,17 +36,18 @@ fixedOperators.forEach(operator => operatorDefinitions.set(operators[operator], 
  * "immediateFrom" decides which expressions are parsed as functions to be invoked, or as the computed result,
  * "context" decides whether the operator use "$.accumulator" and "$.current" instead of "@",
  * "defaults" can fill in default values for the operator's parameters.
+ * "async" allows expressions to be built with the asyncEngine, but will be given to the operator as a Promise<Func>.
  * @param {(...args: any[]) => ((source: any) => import('rxjs').Observable<any>) | import('rxjs').OperatorFunction<any, any> | import('rxjs').MonoTypeOperatorFunction<any> } operator
- * @param {{ immediateFrom?: number, context?: boolean, defaults?: any[], parseDefaults?: boolean, defaultStart?: number }} [options]
+ * @param {{ immediateFrom?: number, context?: boolean, defaults?: any[], parseDefaults?: boolean, defaultStart?: number, async?: boolean }} [options]
  * @param {boolean} [inject] Decides whether this should be injected into a DSL-wide configuration, or wrap the operator. If you are outside of the scope of the module, you might use false.
  */
-export function declare (operator, { immediateFrom = 1, context = false, defaults = [], parseDefaults = false, defaultStart = 0 } = {}, inject = true) {
+export function declare (operator, { immediateFrom = 1, context = false, defaults = [], parseDefaults = false, defaultStart = 0, async = false } = {}, inject = true) {
   if (parseDefaults) defaults = defaults.map(generateLogic)
 
-  if (inject) operatorDefinitions.set(operator, { immediateFrom, context, defaults, defaultStart })
+  if (inject) operatorDefinitions.set(operator, { immediateFrom, context, defaults, defaultStart, async })
   return {
     operator,
-    configuration: { immediateFrom, context, defaults, defaultStart }
+    configuration: { immediateFrom, context, defaults, defaultStart, async }
   }
 }
 
@@ -74,6 +75,9 @@ function buildOperator (name, expressions, { asyncEngine, n = 1, eval: evaluate 
   const definition = operatorDefinitions.get(operator) || operator?.configuration || { immediateFrom: +!evaluate }
   const operatorFunc = operator?.operator || operator
 
+  // a hack due to this being a truly virtual operator.
+  if (name === 'async') definition.async = true
+
   if (n === 1 && definition.context) {
     mutateTraverse(expressions[0], i => {
       if (typeof i.var !== 'undefined') throw new Error('Do not use the typical "@" operator in a reducer. Use $.current and $.accumulator.')
@@ -84,17 +88,16 @@ function buildOperator (name, expressions, { asyncEngine, n = 1, eval: evaluate 
 
   for (let i = 0; i < expressions.length; i++) {
     expressions[i] = i < definition.immediateFrom
-      ? engine.build(expressions[i])
+      ? definition.async
+          ? asyncEngine.build(expressions[i])
+          : engine.build(expressions[i])
       : engine.run(expressions[i])
   }
   // console.log(name, expressions)
 
   const logic = expressions.shift()
 
-  if (name === 'async') {
-    const f = asyncEngine.build(logic)
-    return operators.mergeMap(async (...args) => (await f)(args), ...expressions)
-  }
+  if (name === 'async') return operators.mergeMap(async (...args) => (await logic)(...args), ...expressions)
 
   if (!operator) throw new Error(`Operator '${name}' has not been exposed to the DSL.`)
 
