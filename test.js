20166e74917e7f40b21124600ebf4e3b3f50d274
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2023-09-14 09:40:02 -0500

    Update dependencies

--- test.js
+++ test.js
@@ -7,16 +7,14 @@ function * gen (count = Infinity) {
   while (i++ < count) {
     // if(!(i%10000)) await new Promise(resolve => setTimeout(resolve, 1))
     // yield { date: new Date(new Date('2020-05-03').getTime() + ((Math.random() * 1e11)| 0)), age: Math.random() * 80    }
-    yield [Math.random() * 50, Math.random() * 30]
+    yield { name: 'Emily', age: Math.random() * 90, Team: Math.floor(Math.random() * 10) }
   }
 }
 
 console.time('Pipeline')
 from(gen()).pipe(...dsl(` 
-  take 1e6
-  flush 50ms, true >>
-    toArray
-  <<
+  take 1
+  map { ...@, Math.floor(@.age) + 1  }
 `)
 ).subscribe({
   next: console.log,
